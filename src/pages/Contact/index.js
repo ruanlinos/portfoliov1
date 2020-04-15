06a1e70962007d2ebecd675f073ef25354fc21c6
Ruan Linos Alves <ruanlinos@Ruans-iMac.local>
2020-04-14 17:00:23 -0700

    fix: fixed email and whatsapp phone number

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -9,13 +9,13 @@ export default function Contact() {
     <Layout center id="contact">
       <Title>Get in touch</Title>
       <Container>
-        <Link target="_blank" href="mailto:contatomerisio@gmail.com">
+        <Link target="_blank" href="mailto:ruandeveloper@hotmail.com">
           <FaRegEnvelope size={16} />
           ruandeveloper@hotmail.com
         </Link>
         <Link
           target="_blank"
-          href="https://api.whatsapp.com/send?phone=5542984160407&text=Hey!%20I%20have%20some%20nice%20projects%20and%20I%20think%20you%20can%20help%20me.%20Can%20we%20talk%3F"
+          href="https://api.whatsapp.com/send?phone=+5542984160407&text=Hey!%20I%20have%20some%20nice%20projects%20and%20I%20think%20you%20can%20help%20me.%20Can%20we%20talk%3F"
         >
           <FaWhatsapp size={16} />
           Feel free to send me a message.
